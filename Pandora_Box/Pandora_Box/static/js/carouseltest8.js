0278efdd5f8159f4672622fd9bea66fa9072d3f2
shoto87 <161234471+shoto87@users.noreply.github.com>
2024-03-15 22:30:12 +0530

    added game slates

---
+++ Pandora_Box/Pandora_Box/static/js/carouseltest8.js
@@ -0,0 +1,9 @@
+function change(element) {
+    var current = element.style.color;
+
+    if (current === 'black')
+        element.style.color = 'red';
+
+    else
+        element.style.color = 'black';
+}
